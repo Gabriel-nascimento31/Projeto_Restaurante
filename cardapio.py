bd37a72afd2cdd2e93075c73cd8f7736d881cdab
Gabriel-nascimento31 <dogabriel00@gmail.com>
2026-09-12 20:35:42 -0300

    Update cardapio.py

--- cardapio.py
+++ cardapio.py
@@ -4,17 +4,96 @@ from lista_encadeada import Lista
 
 cardapio = Lista()
 
-
+    
 item1 = Item()
+item1.nome = 'macarronada'
+item1.preco = 10.00
+item1.tipo = 'refeiçao'
+
 item2 = Item()
+item2.nome = 'arroz-feijão'
+item2.preco = 5.00
+item2.tipo = 'refeição'
+
 item3 = Item()
+item3.nome = 'lazanha'
+item3.preco = 20.00
+item3.tipo = 'refeição'
+
 item4 = Item()
+item4.nome = 'nhoque'
+item4.preco = 15.00
+item4.tipo = 'refeição'
+
 item5 = Item()
+item5.nome = 'filé de frango'
+item5.preco = 12.00
+item5.tipo = 'refeição'
+
 item6 = Item()
+item6.nome = 'bife frito'
+item6.preco = 25.00
+item6.tipo = 'refeição'
+
 item7 = Item()
+item7.nome = 'almondega'
+item7.preco = 18.00
+item7.tipo = 'refeição'
+
 item8 = Item()
+item8.nome = 'batata frita'
+item8.preco = 8.00
+item8.tipo = 'refeição'
+
 item9 = Item()
+item9.nome = 'polenta'
+item9.preco = 15.00
+item9.tipo = 'refeição'
+
 item10 = Item()
+item10.nome = 'carne moida'
+item10.preco = 18.00
+item10.tipo = 'refeição'
+
+item11 = Item()
+item11.nome = 'hamburguer'
+item11.preco = 20.00
+item11.tipo = 'refeição'
+
+item12 = Item()
+item12.nome = 'pastel'
+item12.preco = 11.00
+item12.tipo = 'refeição'
+
+item13 = Item()
+item13.nome = 'frango assado'
+item13.preco = 30.00
+item13.tipo = 'refeição'
+
+item14 = Item()
+item14.nome = 'salada'
+item14.preco = 5.00
+item14.tipo = 'refeição'
+
+item15 = Item()
+item15.nome = 'estrogonofe'
+item15.preco = 20.00
+item15.tipo = 'refeição'
+
+item16 = Item()
+item16.nome = 'refrigerante'
+item16.preco = 8.00
+item16.tipo = 'bebida'
+
+item17 = Item()
+item17.nome = 'suco'
+item17.preco = 5.00
+item17.tipo = 'bebida'
+
+item18 = Item()
+item18.nome = 'agua com gás'
+item18.preco = 3.00
+item18.tipo = 'bebida'
 
 
 cardapio.adicionar(item1)
@@ -27,13 +106,12 @@ cardapio.adicionar(item7)
 cardapio.adicionar(item8)
 cardapio.adicionar(item9)
 cardapio.adicionar(item10)
+cardapio.adicionar(item11)
+cardapio.adicionar(item12)
+cardapio.adicionar(item13)
+cardapio.adicionar(item14)
+cardapio.adicionar(item15)
+cardapio.adicionar(item16)
+cardapio.adicionar(item17)        
+cardapio.adicionar(item18)      
 
-
-
-
-
-
-         
-
-        
-        
